f30b7c4064099f844482d481988a77b4a79f0ed4
Miroslav P <miroslav.petkevic@gmail.com>
2019-01-25 13:19:29 +0200

    first commit

---
+++ src/components/layout/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Sidebar from './Sidebar';
+import Clients from '../clients/Clients';
+
+export default function() {
+  return (
+    <div className="row">
+      <div className="col-md-10">
+        <Clients />
+      </div>
+      <div className="col-md-2">
+        <Sidebar />
+      </div>
+    </div>
+  );
+}
